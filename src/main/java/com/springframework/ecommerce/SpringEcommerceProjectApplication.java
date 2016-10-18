25d5ad2e61b3b0cf562af605a29146fc22f90864
Steven McDonald <engineers2526@gmail.com>
2016-10-17 23:08:01 -0500

    Initial commit of directory.

---
+++ src/main/java/com/springframework/ecommerce/SpringEcommerceProjectApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceProjectApplication.class, args);
+	}
+}
